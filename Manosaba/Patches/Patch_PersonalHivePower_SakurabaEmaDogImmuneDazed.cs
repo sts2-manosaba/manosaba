07e3d0d34bf66ba9ae92bb9e0057684f7b54eecb
Clione <clioneka@gmail.com>
2026-04-04 10:48:13 +0800

    fix: ema attack entomancer

--- Manosaba/Patches/Patch_PersonalHivePower_SakurabaEmaDogImmuneDazed.cs
+++ Manosaba/Patches/Patch_PersonalHivePower_SakurabaEmaDogImmuneDazed.cs
@@ -2,19 +2,20 @@ using HarmonyLib;
 using Manosaba.Characters.Common.Monsters;
 using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.Models.Powers;
-using MegaCrit.Sts2.Core.ValueProps;
+using System.Threading.Tasks;
 
 [HarmonyPatch(typeof(PersonalHivePower), nameof(PersonalHivePower.AfterDamageReceived))]
 public static class Patch_PersonalHivePower_SakurabaEmaDogImmuneDazed
 {
-    private static bool Prefix(PersonalHivePower __instance, Creature target, ValueProp props, Creature? dealer)
+    private static bool Prefix(PersonalHivePower __instance, Creature target, Creature? dealer, ref Task __result)
     {
-        // Keep default behavior unless this is exactly Personal Hive retaliating to Sakuraba Ema Dog's powered attack.
-        if (target == __instance.Owner && dealer?.Monster is SakurabaEmaDog)
+        // Keep default behavior unless this is exactly Personal Hive retaliating to Sakuraba Ema Dog.
+        if (target != __instance.Owner || dealer?.Monster is not SakurabaEmaDog)
         {
-            return false;
+            return true;
         }
 
-        return true;
+        __result = Task.CompletedTask;
+        return false;
     }
 }
