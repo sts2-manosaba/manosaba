ae74d9c3a17ce7683f898d3363f2c127214c360d
Clione <clioneka@gmail.com>
2026-04-06 14:39:27 +0800

    add: YukiBackflip

---
+++ Manosaba/Characters/Common/Cards/YukiBackflip.cs
@@ -0,0 +1,40 @@
+using BaseLib.Utils;
+using manosaba.Characters.Common;
+using Manosaba.Extensions;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace Manosaba.Characters.Common.Cards
+{
+    [Pool(typeof(CommonCardPool))]
+    public class YukiBackflip : PathCustomCardModel
+    {
+        public override bool GainsBlock => true;
+
+        private const int energyCost = 1;
+        private const CardType type = CardType.Skill;
+        private const CardRarity rarity = CardRarity.Common;
+        private const TargetType targetType = TargetType.Self;
+        private const bool shouldShowInCardLibrary = true;
+
+        protected override IEnumerable<DynamicVar> CanonicalVars => [new BlockVar(5, ValueProp.Move), new CardsVar(2)];
+
+        public YukiBackflip() : base(energyCost, type, rarity, targetType, shouldShowInCardLibrary)
+        {
+        }
+
+        protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+        {
+            await CreatureCmd.GainBlock(Owner.Creature, DynamicVars.Block, cardPlay);
+            await CardPileCmd.Draw(choiceContext, DynamicVars.Cards.BaseValue, Owner);
+        }
+
+        protected override void OnUpgrade()
+        {
+            DynamicVars.Block.UpgradeValueBy(3m);
+        }
+    }
+}
