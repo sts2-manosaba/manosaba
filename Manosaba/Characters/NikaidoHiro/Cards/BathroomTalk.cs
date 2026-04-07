848b75d5593482c78c33112d550619e9fecf373d
Clione <clioneka@gmail.com>
2026-04-07 18:38:33 +0800

    add: Bathroom Talk

---
+++ Manosaba/Characters/NikaidoHiro/Cards/BathroomTalk.cs
@@ -0,0 +1,41 @@
+﻿using BaseLib.Utils;
+using manosaba.Characters.NikaidoHiro;
+using Manosaba.Characters.Common.Monsters;
+using Manosaba.Characters.Common.Powers;
+using Manosaba.Extensions;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+
+namespace Manosaba.Characters.NikaidoHiro.Cards
+{
+    [Pool(typeof(NikaidoHiroCardPool))]
+    public class BathroomTalk : PathCustomCardModel
+    {
+        private const int energyCost = 1;
+        private const CardType type = CardType.Skill;
+        private const CardRarity rarity = CardRarity.Uncommon;
+        private const TargetType targetType = TargetType.Self;
+        private const bool shouldShowInCardLibrary = true;
+
+        protected override IEnumerable<DynamicVar> CanonicalVars => [new SummonVar(8), new PowerVar<VotePower>(1)];
+        protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.FromPower<VotePower>()];
+
+        public BathroomTalk() : base(energyCost, type, rarity, targetType, shouldShowInCardLibrary)
+        {
+        }
+
+        protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+        {
+            await SakurabaEmaDogCmd.Summon(choiceContext, Owner, DynamicVars.Summon.BaseValue, this);
+            await PowerCmd.Apply<VotePower>(Owner.Creature, DynamicVars["VotePower"].BaseValue, Owner.Creature, this);
+        }
+
+        protected override void OnUpgrade()
+        {
+            DynamicVars.Summon.UpgradeValueBy(4);
+        }
+    }
+}
