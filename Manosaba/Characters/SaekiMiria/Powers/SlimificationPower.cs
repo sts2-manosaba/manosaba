a162a1b6821409565179ac2f5fb768edaa7f8830
Zachary Chang <zxc8207@g.rit.edu>
2026-05-02 00:41:35 +0800

    change the effect of SlimificationPower

--- Manosaba/Characters/SaekiMiria/Powers/SlimificationPower.cs
+++ Manosaba/Characters/SaekiMiria/Powers/SlimificationPower.cs
@@ -4,6 +4,7 @@ using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.Entities.Powers;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.Models.Powers;
 using MegaCrit.Sts2.Core.ValueProps;
@@ -12,12 +13,35 @@ namespace Manosaba.Characters.SaekiMiria.Powers;
 
 public sealed class SlimificationPower : PathCustomPowerModel
 {
+    private const string RegenGainVar = "RegenGain";
+
     public override PowerType Type => PowerType.Buff;
     public override PowerStackType StackType => PowerStackType.Counter;
     public override bool AllowNegative => false;
     public override int DisplayAmount => Amount;
 
     protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.FromPower<RegenPower>()];
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new DynamicVar(RegenGainVar, 1m)];
+
+    public override Task AfterApplied(Creature? applier, CardModel? cardSource)
+    {
+        SyncVars();
+        return Task.CompletedTask;
+    }
+
+    public override Task AfterPowerAmountChanged(PowerModel power, decimal amount, Creature? applier, CardModel? cardSource)
+    {
+        _ = amount;
+        _ = applier;
+        _ = cardSource;
+
+        if (ReferenceEquals(power, this))
+        {
+            SyncVars();
+        }
+
+        return Task.CompletedTask;
+    }
 
     public override async Task AfterDamageReceived(
         PlayerChoiceContext choiceContext,
@@ -35,10 +59,17 @@ public sealed class SlimificationPower : PathCustomPowerModel
         if (!props.HasFlag(ValueProp.Move))
             return;
 
-        if (result.UnblockedDamage <= 0)
+        if (!result.WasFullyBlocked || result.BlockedDamage <= 0)
             return;
 
         await PowerCmd.Apply<RegenPower>(Owner, Amount, Owner, cardSource);
     }
-}
 
+    private void SyncVars()
+    {
+        if (DynamicVars.TryGetValue(RegenGainVar, out DynamicVar? regenGain))
+        {
+            regenGain.BaseValue = Amount;
+        }
+    }
+}
