cba993c0e58607cdbf35b6509722664365c77980
Clione <clioneka@gmail.com>
2026-05-05 22:58:37 +0800

    add: accomplice & incitement

---
+++ Manosaba/Characters/HikamiMeruru/Cards/Incitement.cs
@@ -0,0 +1,65 @@
+using BaseLib.Utils;
+using manosaba.Characters.HikamiMeruru;
+using Manosaba.Characters.Common.Powers;
+using Manosaba.Extensions;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+
+namespace Manosaba.Characters.HikamiMeruru.Cards;
+
+[Pool(typeof(HikamiMeruruCardPool))]
+public class Incitement : PathCustomCardModel
+{
+    private const int energyCost = 1;
+    private const CardType type = CardType.Skill;
+    private const CardRarity rarity = CardRarity.Common;
+    private const TargetType targetType = TargetType.AnyPlayer;
+    private const bool shouldShowInCardLibrary = true;
+
+    protected override IEnumerable<IHoverTip> ExtraHoverTips =>
+    [
+        HoverTipFactory.FromPower<MajokaPower>(),
+        base.EnergyHoverTip,
+    ];
+
+    protected override IEnumerable<DynamicVar> CanonicalVars =>
+    [
+        new PowerVar<MajokaPower>(10),
+        new CardsVar(2),
+        new EnergyVar(1),
+    ];
+
+    public Incitement() : base(energyCost, type, rarity, targetType, shouldShowInCardLibrary)
+    {
+    }
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        if (Owner is not { } owner || cardPlay.Target?.Player is not { } targetPlayer)
+        {
+            return;
+        }
+
+        if (targetPlayer == owner)
+        {
+            await CardPileCmd.Draw(choiceContext, DynamicVars.Cards.BaseValue, targetPlayer);
+            return;
+        }
+
+        await PowerCmd.Apply<MajokaPower>(
+            targetPlayer.Creature,
+            DynamicVars["MajokaPower"].BaseValue,
+            owner.Creature,
+            this);
+        await PlayerCmd.GainEnergy(DynamicVars.Energy.BaseValue, targetPlayer);
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars["MajokaPower"].UpgradeValueBy(5);
+        DynamicVars.Cards.UpgradeValueBy(1);
+    }
+}
---
+++ Manosaba/Characters/NikaidoHiro/Cards/Accomplice.cs
@@ -0,0 +1,45 @@
+using BaseLib.Utils;
+using manosaba.Characters.NikaidoHiro;
+using Manosaba.Characters.Common.Monsters;
+using Manosaba.Characters.Common.Powers;
+using Manosaba.Extensions;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace Manosaba.Characters.NikaidoHiro.Cards
+{
+    [Pool(typeof(NikaidoHiroCardPool))]
+    public class Accomplice : PathCustomCardModel
+    {
+        public override bool GainsBlock => true;
+
+        private const int energyCost = 2;
+        private const CardType type = CardType.Skill;
+        private const CardRarity rarity = CardRarity.Rare;
+        private const TargetType targetType = TargetType.Self;
+        private const bool shouldShowInCardLibrary = true;
+
+        protected override IEnumerable<DynamicVar> CanonicalVars => [
+            new SummonVar(7),
+            new BlockVar(8, ValueProp.Move),
+            new PowerVar<SusPower>(2)
+        ];
+
+        protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.FromPower<SusPower>()];
+
+        public Accomplice() : base(energyCost, type, rarity, targetType, shouldShowInCardLibrary)
+        {
+        }
+
+        protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+        {
+            await SakurabaEmaDogCmd.Summon(choiceContext, Owner, DynamicVars.Summon.BaseValue, this);
+            await CreatureCmd.GainBlock(Owner.Creature, DynamicVars.Block, cardPlay);
+            await PowerCmd.Apply<SusPower>(Owner.Creature, DynamicVars["SusPower"].BaseValue, Owner.Creature, this);
+        }
+    }
+}
