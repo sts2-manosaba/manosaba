5b71a08e91ef3303edbc6a097f33d500660c1b01
Clione <clioneka@gmail.com>
2026-06-30 21:06:05 +0800

    fix: Noah paintRefill img

--- Manosaba/Characters/JogasakiNoah/Powers/PaintRefillPower.cs
+++ Manosaba/Characters/JogasakiNoah/Powers/PaintRefillPower.cs
@@ -1,10 +1,7 @@
-using manosaba.Extensions;
-using Manosaba.Characters.JogasakiNoa.Orbs;
 using Manosaba.Extensions;
 using MegaCrit.Sts2.Core.Combat;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Creatures;
-using MegaCrit.Sts2.Core.Entities.Orbs;
 using MegaCrit.Sts2.Core.Entities.Powers;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Models;
@@ -18,10 +15,6 @@ public class PaintRefillPower : PathCustomPowerModel
     public override PowerType Type => PowerType.Buff;
     public override PowerStackType StackType => PowerStackType.Single;
     public override bool AllowNegative => false;
-
-    public override string CustomPackedIconPath => "power.png".PowerImagePath();
-    public override string CustomBigIconPath => "power.png".PowerImagePath();
-    public override string CustomBigBetaIconPath => "power.png".PowerImagePath();
 
     public override async Task AfterSideTurnEnd(PlayerChoiceContext choiceContext, CombatSide side, IEnumerable<Creature> creatures)
     {
